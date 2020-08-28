9cca9ee191db0046896db5593cb750586a643b98
Marlon Pacheco <pacheco.mjc@gmail.com>
2020-08-27 20:52:34 -0500

    finished the JS file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//module requirements
 var inquirer = require("inquirer");
+var fs = require("fs");
 
+//inquirer module to gather user responses
 inquirer
   .prompt([
     {
@@ -18,43 +21,92 @@ inquirer
       name: "instructions"
     },
     {
-        type: "input",
-        message: "Enter usage information",
-        name: "usage"
+      type: "input",
+      message: "Enter usage information",
+      name: "usage"
     },
     {
-        type: "input",
-        message: "Enter contribution guidelines",
-        name: "contribution"
+      type: "input",
+      message: "Enter contribution guidelines",
+      name: "contribution"
     },
     {
-        type: "input",
-        message: "Enter test instructions",
-        name: "test"
+      type: "input",
+      message: "Enter test instructions",
+      name: "test"
     },
     {
-        type: "list",
-        message: "Choose license type",
-        choices: ["license1", "2", "3"],
-        name: "license"
+      type: "list",
+      message: "Choose license type",
+      choices: ["Apache", "GPL", "MIT"],
+      name: "license"
     },
     {
-        type: "input",
-        message: "Enter your GitHub username",
-        name: "github"
+      type: "input",
+      message: "Enter your GitHub username",
+      name: "github"
     },
     {
-        type: "input",
-        message: "Enter your email address",
-        name: "email"
+      type: "input",
+      message: "Enter your email address",
+      name: "email"
     },
   ])
-  .then(function(response) {
-console.log(response)
-    // if (response.confirm === response.password) {
-    //   console.log("Success!");
-    // }
-    // else {
-    //   console.log("You forgot your password already?!");
-    // }
+
+  //function to extract/use responses
+  .then(function (response) {
+    console.log(response)
+
+    //if statement to determine link to license bade
+    licenseType = "";
+    if (response.license === "Apache") {
+      licenseType = "[![License](https://img.shields.io/badge/License-Apache%202.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)"
+    } else if (response.license === "GPL") {
+      licenseType = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+    } else { licenseType = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" }
+    ;
+
+    //const for the text contents of the README file
+    const createFile = `
+# ${response.title} ${licenseType}
+${response.description}
+
+## Table of Contents
+ - [Installation Instructions](#installation-instructions)
+ - [Usage](#usage)
+ - [Contributions](#Contributions)
+ - [Test](#Test)
+ - [Questions](#Questions)
+
+## Installation Instructions
+${response.instructions}
+
+## Usage
+${response.usage}
+
+## Contributions
+${response.contribution}
+
+## Test
+${response.test}
+
+## Questions
+For questions and feedback
+My GitHub: https://github.com/${response.github}
+
+Email: ${response.email}
+    `
+
+    //fs module to create the projectREADME.md file
+    fs.writeFile("projectREADME.md", createFile, function (err) {
+
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log("Success!");
+
+    });
   });
+
+
---
+++ test.md
@@ -0,0 +1,30 @@
+
+# asdfasdfa [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+asdfasdff
+
+## Table of Contents
+ - [Installation Instructions](#installation-instructions)
+ - [Usage](#usage)
+ - [Contributions](#Contributions)
+ - [Test](#Test)
+ - [Questions](#Questions)
+
+## Installation Instructions
+asdfasdfasf
+
+## Usage
+sadfafdsdasf
+
+## Contributions
+asdfasdfasdf
+
+## Test
+asdfasdfasdf
+
+## Questions
+For questions and feedback
+My GitHub: https://github.com/asdfasfdasf
+
+Email: asdfasfd
+
+    
